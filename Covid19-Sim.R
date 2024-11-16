5241c909e39625ab8e49c763ee0ac776818f9fb8
Anisha Mariam Abraham <155948561+xanishax@users.noreply.github.com>
2024-11-17 08:38:46 +1030

    Update Covid19-Sim.R

--- Covid19-Sim.R
+++ Covid19-Sim.R
@@ -1,269 +1,125 @@
 # Clean up all variables
 rm(list = ls())
 
-# Libraries to load
-library(tidyverse)
-library(psych)
+# Define the basic parameters
+N_population <- 10000 # Population size
+N_days <- 100 # Number of simulation days
+initial_sick <- 2  # Number of initially infected citizens
+meet_people <- 20  # Maximum number of people a sick person can meet
+infection_rate <- 0.3  # Base infection rate
+infection_period <- 10  # Number of days a sick person remains infectious
+mortality_rate <- 0.005 / infection_period  # Mortality rate
+immunity_coeff <- 0.1  # Immunity coefficient
 
-# Files to be read
-maxTempFilename = "IDCJAC0010_023034_1800_Data.csv"
-maxTempData = readr::read_csv(maxTempFilename)
+# Create initial health state and day count
+health_state <- rep(0, times = N_population)  # 0 represents healthy
+day_count <- rep(0, times = N_population)
 
-# Dimensions
-dim(maxTempData) 
+# Randomly mark initial sick citizens
+initial_sick_indices <- sample(1:N_population, size = initial_sick)
+health_state[initial_sick_indices] <- 1  # 1 represents sick
 
-# Column names
-colnames(maxTempData)
+# Create an empty data frame to store daily results
+results_df <- data.frame(
+  Day = integer(0),
+  TotalHealthy = integer(0),
+  TotalSick = integer(0),
+  DailyDeaths = integer(0),
+  TotalDeaths = integer(0),
+  NewInfections = integer(0)
+)
 
-minTempFilename = "IDCJAC0011_023034_1800_Data.csv"
-minTempData = readr::read_csv(minTempFilename)
+# Initialize vectors to store historical data
+history_total_sick <- integer(0)
+history_total_healthy <- integer(0)
+history_total_deaths <- integer(0)
+previous_day_deaths <- 0  # Initialize the previous day's sick count to 0
 
-# Dimensions
-dim(minTempData) 
+# Initialize a vector to store daily death counts
+daily_deaths <- integer(0)
 
-# Column names
-colnames(minTempData)
-
-# Join year, month, and day columns into a single date column
-maxTempData$Date <- as.Date(paste(maxTempData$Year, maxTempData$Month, 
-                                  maxTempData$Day, sep = "-"))
-
-minTempData$Date <- as.Date(paste(minTempData$Year, minTempData$Month, 
-                                  minTempData$Day, sep = "-"))
-
-# Convert months to month names
-maxTempData$Month <- month.name[as.numeric(maxTempData$Month)]
-minTempData$Month <- month.name[as.numeric(minTempData$Month)]
-
-# Convert Day values to numeric type
-maxTempData$Day <- as.numeric(maxTempData$Day)
-minTempData$Day <- as.numeric(minTempData$Day)
-
-# Identify rows with complete cases (no missing values) in Temperature column
-maxTempData <- maxTempData[complete.cases(maxTempData[, c("Maximum temperature (Degree C)")]), ]
-minTempData <- minTempData[complete.cases(minTempData[, c("Minimum temperature (Degree C)")]), ]
-
-# Group the data by year and find the highest maximum temperature
-maxTempEachYear <- maxTempData %>%
-  group_by(Year) %>%
-  filter(`Maximum temperature (Degree C)` == max(`Maximum temperature (Degree C)`)) %>%
-  select(Year, Month, `Maximum temperature (Degree C)`)
-
-# Find the maximum temperature across all years
-max_temp <- max(maxTempData$`Maximum temperature (Degree C)`)
-
-# Find the year corresponding to the maximum temperature
-year_with_max_temp <- maxTempData$Year[maxTempData$`Maximum temperature (Degree C)` == max_temp]
-
-# Display the maximum temperature and the corresponding year
-cat("Maximum Temperature:", max_temp, "°C\n")
-cat("Year with Maximum Temperature:", year_with_max_temp, "\n")
-
-maxTempSummary <- maxTempData %>%
-  group_by(Year, Month) %>%
-  summarize(Max_Temperature = mean(`Maximum temperature (Degree C)`))
-
-# Reorder the Month variable in calendar order
-maxTempSummary$Month <- factor(maxTempSummary$Month, levels = month.name)
-
-# Create the line plot
-ggplot(maxTempSummary, aes(x = Year, y = Max_Temperature, color = Month)) +
-  geom_line() +
-  labs(
-    title = "Mean Maximum Temperatures by Month and Year",
-    x = "Year",
-    y = "Maximum Temperature (°C)"
-  ) +
-  facet_wrap(~Month, ncol = 4) +
-  theme_light() +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(
-    text = element_text(size = 14)  
+# Simulate the pandemic
+for (day in 1:N_days) {
+  
+  # Extract relevant indices for sick individuals and increment day of sickness count
+  sick_indices <- which(health_state == 1)
+  day_count[sick_indices] <- day_count[sick_indices] + 1
+  
+  # Determine mortality using a function to generate random numbers
+  mortality <- runif(length(sick_indices)) < mortality_rate
+  health_state[sick_indices[mortality]] <- 2  # 2 represents dead
+  recovered_indices <- sick_indices[!mortality & day_count[sick_indices] > infection_period]
+  health_state[recovered_indices] <- 3  # 3 represents immune
+  day_count[recovered_indices] <- 0
+  
+  # Determine who meets whom
+  num_to_meet_matrix <- sample(0:meet_people, size = length(sick_indices), replace = TRUE)
+  num_random_meetings <- sum(num_to_meet_matrix) # Total number of meetings
+  meet_indices <- sample(N_population, size = num_random_meetings, replace = TRUE) # Indices to be met
+  meet_health <- health_state[meet_indices]
+  
+  # Filter to select healthy and immune individuals meeting sick people
+  idx_to_infect <- meet_indices[meet_health == 0 | meet_health == 3]
+  
+  # Determine infection using a function to generate random numbers and consider immunity
+  infection_probs <- runif(length(idx_to_infect))
+  immune_individuals <- health_state[idx_to_infect] == 3
+  adjusted_infection_rate <- ifelse(immune_individuals, infection_rate * immunity_coeff, infection_rate)
+  infection_success <- infection_probs < adjusted_infection_rate
+  
+  # Update health state for newly infected individuals
+  new_infections <- idx_to_infect[infection_success]
+  health_state[new_infections] <- 1
+  
+  # Calculate daily statistics
+  total_healthy <- sum(health_state %in% c(0, 3))
+  total_sick <- sum(health_state == 1)
+  total_deaths <- sum(health_state == 2)
+  new_infections <- length(new_infections)
+  
+  # Calculate daily deaths by subtracting total deaths from previous day's deaths
+  daily_death_count <- total_deaths - previous_day_deaths
+  # Update the previous day's deaths count for the next iteration
+  previous_day_deaths <- total_deaths
+  
+  # Remove dead people from population
+  N_population <- N_population - daily_death_count
+  
+  # Append daily results to the data frame
+  results_df <- rbind(
+    results_df,
+    data.frame(Day = day, TotalHealthy = total_healthy, TotalSick = total_sick, 
+               TotalDeaths = total_deaths, NewInfections = new_infections,
+               DailyDeaths = daily_death_count)
   )
-
-# Group the data by year and find the lowest minimum temperature
-minTempEachYear <- minTempData %>%
-  group_by(Year) %>%
-  filter(`Minimum temperature (Degree C)` == min(`Minimum temperature (Degree C)`)) %>%
-  select(Year, Month, `Minimum temperature (Degree C)`)
-
-# Find the minimum temperature across all years
-min_temp <- min(minTempData$`Minimum temperature (Degree C)`)
-
-# Find the year corresponding to the maximum temperature
-year_with_min_temp <- minTempData$Year[minTempData$`Minimum temperature (Degree C)` == min_temp]
-
-# Display the maximum temperature and the corresponding year
-cat("Minimum Temperature:", min_temp, "°C\n")
-cat("Year with Minimum Temperature:", year_with_min_temp, "\n")
-
-minTempSummary <- minTempData %>%
-  group_by(Year, Month) %>%
-  summarize(Min_Temperature = mean(`Minimum temperature (Degree C)`))
-
-# Reorder the Month variable in calendar order
-minTempSummary$Month <- factor(minTempSummary$Month, levels = month.name)
-
-# Create the line plot
-ggplot(minTempSummary, aes(x = Year, y = Min_Temperature, color = Month)) +
-  geom_line() +
-  labs(
-    title = "Mean Minimum Temperatures by Month and Year",
-    x = "Year",
-    y = "Minimum Temperature (°C)"
-  ) +
-  facet_wrap(~Month, ncol = 4) +
-  theme_light() +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(
-    text = element_text(size = 14) 
-  )
-
-# Function to calculate and present descriptive statistics for any given month and year
-calculateDescriptiveStats <- function(data, years, months) {
-  data %>%
-    filter(Year %in% years, Month %in% months) %>%
-    group_by(Year, Month) %>%
-    summarise(
-      Mean = mean(`Maximum temperature (Degree C)`, na.rm = TRUE),
-      SD = sd(`Maximum temperature (Degree C)`, na.rm = TRUE),
-      Skewness = skew(`Maximum temperature (Degree C)`, na.rm = TRUE),
-      Kurtosis = kurtosi(`Maximum temperature (Degree C)`, na.rm = TRUE),
-      Median = median(`Maximum temperature (Degree C)`, na.rm = TRUE),
-      IQR = IQR(`Maximum temperature (Degree C)`, na.rm = TRUE)
-    ) %>%
-    mutate(across(where(is.numeric), ~round(., 2))) %>%  # Round numeric columns
-    ungroup()
+  # Update historical data
+  history_total_sick <- c(history_total_sick, total_sick)
+  history_total_healthy <- c(history_total_healthy, total_healthy)
+  history_total_deaths <- c(history_total_deaths, total_deaths)
 }
+print(results_df)
 
-# Example usage: Analyze for multiple years and months
-years_to_analyze <- c(1961, 1971, 1981, 1991, 2001, 2011, 2021)
-months_to_analyze <- c('January', 'July') 
+# Plot the history of infection
+plot(results_df$Day, history_total_sick, type = "l", col = "red", xlab = "Day", ylab = "Number of People", main = "History of Infection")
+lines(results_df$Day, history_total_healthy, col = "blue")
+lines(results_df$Day, history_total_deaths, col = "green")
 
-descriptiveStats <- calculateDescriptiveStats(maxTempData, years_to_analyze, months_to_analyze)
-print(descriptiveStats)
+# Add a legend at the custom "middle right" position
 
-# Convert Year to a factor to treat it as categorical
-descriptiveStats$Year <- factor(descriptiveStats$Year)
+legend('topright', legend = c("Cumulative Sick", "Cumulative Healthy", "Cumulative Dead"), col = c("red", "blue", "green"), lty = 1)
 
-# Create the line graph to study means
-ggplot(descriptiveStats, aes(x = Year, y = Mean, fill = Month)) +
-  geom_line(color = "#69b3a2", linewidth = 1, alpha = 0.9, linetype = 2, group = 1) +
-  geom_point(aes(label = round(Mean, 2)), color = "red", size = 3) +  
-  geom_text(aes(label = round(Mean, 2)), vjust = -1) +  
-  labs(
-    title = "Study of Mean Maximum Temperature over Six Decades",
-    x = "Year",
-    y = "Mean Maximum Temperature (°C)"
-  ) +
-  facet_wrap(~Month, ncol = 1) +
-  theme_light() +
-  ylim(0, 35) +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    text = element_text(size = 12)  
-  ) +
-  guides(fill = FALSE)  # Remove the legend
+# A higher mortality rate can deter people from engaging in risky behaviors that 
+# contribute to disease transmission (e.g., practicing better hygiene or avoiding 
+# close contact with the sick). If more infected individuals die, they spend less 
+# time in the infectious state, which can reduce the opportunities for transmission. 
+# This, in turn, can slow infection growth. A lower infection rate means that each 
+# infected person is less likely to transmit the disease to others. This can 
+# significantly slow the spread of the disease within the population. If individuals 
+# remain infectious for a longer time, they have more opportunities to transmit the 
+# disease to others, potentially increasing infection growth. On the other hand, if 
+# individuals recover more slowly, they might be isolated or receive medical care, 
+# which can reduce transmission. The net effect depends on other factors. Fewer 
+# interactions between individuals mean that the disease has fewer opportunities 
+# to spread, which can substantially slow down infection growth.
 
 
-# Boxplot to study medians of temperatures
-custom_palette <- c("darkgoldenrod", "darkcyan", "darkmagenta", "darkgreen", "darkred", "darkblue", "red")
-
-filteredData <- maxTempData %>% filter(Month %in% descriptiveStats$Month, Year %in% descriptiveStats$Year)
-
-# Create the boxplots and add dots for individual data points
-ggplot(filteredData, aes(x = Year, y = `Maximum temperature (Degree C)`, fill = Month, color = factor(Year))) +
-  geom_boxplot(position = position_dodge()) +  # Add boxplots
-  geom_jitter() +  # Add data points
-  labs(
-    title = "Comparison of Maximum Temperatures",
-    x = "Year",
-    y = "Temperature (°C)"
-  ) +
-  theme_minimal() +
-  scale_color_manual(values = custom_palette) +
-  scale_fill_manual(values = c("coral", "cadetblue1")) +
-  labs(fill = "Month", color = "Year") +  
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    text = element_text(size = 12)
-  ) 
-
-# Combine two data sets together and calculate a daily range between maximal
-# and minimal temperature
-minMaxTemp <- left_join(maxTempData, 
-                    minTempData %>% 
-                      select(`Minimum temperature (Degree C)`, 
-                             `Days of accumulation of minimum temperature`, 
-                             Date),
-                    by = "Date")  %>%
-  mutate(`Daily Range` = `Maximum temperature (Degree C)` - `Minimum temperature (Degree C)`
-  )
-
-# Filter data for specific years
-filteredData <- minMaxTemp[minMaxTemp$Year %in% years_to_analyze, ]
-filteredData$Month <- factor(filteredData$Month, levels = month.name)
-
-# Create the faceted violin plot
-ggplot(filteredData, aes(x = Month, y = `Daily Range`, fill = Year)) +
-  geom_violin() +
-  labs(
-    title = "Daily Temperature Range by Month and Year",
-    x = "Month",
-    y = "Daily Temperature Range (°C)"
-  ) +
-  theme_light() +
-  facet_wrap(~Year, ncol = 1)   +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    text = element_text(size = 12) 
-  ) 
-
-# Filter the data for temperatures >= 35 degrees
-heatwaveData <- maxTempData %>%
-  filter(`Maximum temperature (Degree C)` >= 35)
-
-# Group the data by year and count the number of days per year
-countPerYear <- heatwaveData %>%
-  group_by(Year) %>%
-  summarise(Count = n())
-
-# Create the bar chart
-ggplot(countPerYear, aes(x = Year, y = Count, fill = Year)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  coord_flip() +
-  labs(
-    title = "Number of Days with Max Temperature Above 35°C",
-    x = "Year",
-    y = "Count"
-  ) +
-  theme_minimal() + 
-  geom_text(aes(label = Count), hjust = -0.3, size=3)  +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    text = element_text(size = 12)  
-  ) 
-
-# Filter the data for temperatures <=3 degrees
-coldspellData <- minTempData %>%
-  filter(`Minimum temperature (Degree C)` <= 3)
-
-# Group the data by year and count the number of days per year
-countPerYear <- coldspellData %>%
-  group_by(Year) %>%
-  summarise(Count = n())
-
-ggplot(countPerYear, aes(x = Year, y = Count, fill = Year)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  coord_flip() +
-  labs(
-    title = "Number of Days with Min Temperature Below 4°C",
-    x = "Year",
-    y = "Count"
-  ) +
-  theme_minimal() +
-  geom_text(aes(label = Count), hjust = -0.3, size=3)  +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    text = element_text(size = 12)  
-  ) 
